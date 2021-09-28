d3c5007926dd6e2b6d8a609cf4197d3817515538
Vivek Ragunathan <vivekragunathan@gmail.com>
2021-09-28 15:48:46 -0700

    Updated action 1632869326N

--- actions/init.py
+++ actions/init.py
@@ -2,14 +2,11 @@ import sys
 import os
 from utils import *
 
-if len(sys.argv) <= 1:
-    sys.exit('Required arg not specified')
-else:
-    who_are_u = sys.argv[1]
+who_are_u = '???' if len(sys.argv) <= 1 else sys.argv[1]
 
-    print(
-        f"""{banner('Identification Header')}
-        Hello {who_are_u} * {ts_now()}
-        Env(WHO_ARE_U): {os.environ['WHO_ARE_U']}
-        """
-    )
+print(
+    f"""{banner('Identification Header')}
+    Hello {who_are_u} * {ts_now()}
+    Env(WHO_ARE_U): {os.environ['WHO_ARE_U']}
+    """
+)
