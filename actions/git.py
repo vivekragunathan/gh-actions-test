74d1bd213d2158840905930b12dbcc6b748aee78
Vivek Ragunathan <vivekragunathan@gmail.com>
2021-09-28 16:20:24 -0700

    Updated action 1632871224N

---
+++ actions/git.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+from utils import *
+
+if len(sys.argv) <= 1:
+    sys.exit('git.py: git sha not specified')
+
+__GH_TOKEN__ = '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'
+gh_sha = sys.argv[1]
+
+print_cmd_output(f"echo {__GH_TOKEN__} | gh auth login --with-token", 'Authenticating @ GitHub')
+
+print_cmd_output("gh repo view php-savers")
+
+print_cmd_output(f"git diff-tree --no-commit-id --patch-with-raw -r {gh_sha}", f"Fetching git diff ({gh_sha})")
+
+# echo '>>> List workflows'
+# gh workflow view CI
--- actions/utils.py
+++ actions/utils.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
 from datetime import datetime
+import subprocess
+from typing import Optional
+
 
 def ts_now() -> str:
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
+
 def banner(s: str) -> str:
     return f"✦✦✦ {s} ✦✦✦"
+
+
+def shell_cmd(cmd: str):
+    return subprocess.run(cmd.split(' '), capture_output=True, text=True) # stdout=subprocess.PIPE
+
+
+def print_cmd_output(cmd: str, banner_msg: Optional[str] = None) -> str:
+    if banner:
+        print(banner(banner_msg))
+
+    result = subprocess.getoutput(cmd)
+    print(result)
+    return result
